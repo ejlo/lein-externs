0ac3aa7c03502b6ef160379d7f85d1718e0a671f
Erik Ouchterlony <erik.ouchterlony@bredband.net>
2013-11-01 12:21:50 +0100

    fix the project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-externs "0.1.0"
   :description "Quick extern scraper"
-  :url "http://github.com/rbuchmann"
+  :url "https://github.com/ejlo/lein-externs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
