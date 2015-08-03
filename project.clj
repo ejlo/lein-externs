627cdfd4df13a1487d22139ff0aa6a92bc8eb5bd
Erik Ouchterlony <erik.ouchterlony@bredband.net>
2015-08-03 02:08:17 +0200

    version 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-externs "0.1.4"
+(defproject lein-externs "0.1.5"
   :description "Quick extern scraper"
   :url "https://github.com/ejlo/lein-externs"
   :license {:name "Eclipse Public License"
